fad5c1bd752f1fcfd73a11b532db8f5e88ec2e66
Liliya Smelova <26244983+LiliyaSm@users.noreply.github.com>
2020-09-29 22:44:06 +0300

    feat: pass all tests

--- src/index.js
+++ src/index.js
@@ -37,58 +37,41 @@ const MORSE_TABLE = {
     "-----": "0",
 };
 
-function getValueByMorse(code) {
-    //returns key for value
-    // code = code.split("");
+function getLetterFromMorse(code) {
+    // takes code, format "000000010"
+    // returns letter
     
     if ((code === "**********")) {
         return " ";
     }
+
     for (let i = 0; i < code.length; i++) {
         //get rid of zeros
         if (code[i] === "0" && code[i + 1] === "1") code = code.slice(i + 1);
         break;
     }
-    // code = code.join("");
-    
+       
+    //convert "111011" format to "-.-"
     let result = "";
-    
     for (let i = 0; i < code.length; i = i + 2) {
         if (code[i] + code[i + 1] === "10") {
             result += ".";
         } else if (code[i] + code[i + 1] === "11") {
             result += "-";
         }
-    }
-    
-    console.log("result", result);
+    }    
 
     let value = MORSE_TABLE[result];
-
-    console.log("valuef", value);
-
-    console.log("valuef", value);
-
-    console.log("codef", code);
     return value;
 }
 
 function decode(expr) {
-    // let arr = expr.split("");
     let result = "";
-    // let counter = 0;
-    for (let i = 10; i < expr.length; i = i + 10) {
-        // console.log(expr.length);
-        let code = expr.slice(i - 10, i);
-        console.log("i", i);
-        console.log("counter", i - 10);
-        console.log("code", code);
-
-        let value = getValueByMorse(code);
+    for (let i = 10; i < expr.length + 1; i = i + 10) {
+        //call function for every 10 symbols
+        let value = getLetterFromMorse(expr.slice(i - 10, i));
         result += value;
-        console.log("value", value);
     }
-
     return result;
 }
 
